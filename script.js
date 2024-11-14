4a2df29a9f94618174f8ac981999f0dc1e9434bd
Abel <n01728632@humber.ca>
2024-11-14 02:45:27 -0500

    added event listeners to JS

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const button1 = document.getElementById("button1");
 const button2 = document.getElementById("button2");
-const project1info=  document,getElementById("project1info");
-const project2info=  document,getElementById("project2info");
+const project1info =  document.getElementById("project1info");
+const project2info =  document.getElementById("project2info");
 
-function toggle(){
-    if (projectinfo.styleSheets.display === "none"){
+function toggle(project){
+    const projectinfo = document.getElementById(project);
+    if (projectinfo.style.display === "none"){
         projectinfo.style.display ="block";
     }else{
-        projectinfor.style.display = "none";
+        projectinfo.style.display = "none";
     }
 }
+
+button1.addEventListener("click", function(){
+    toggle("project1info");
+});
+
+button2.addEventListener("click", function(){
+    toggle("project2info");
+});
