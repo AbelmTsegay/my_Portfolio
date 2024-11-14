0b062b16c1af6349c39d3537e26575bfca11104a
Abel <n01728632@humber.ca>
2024-11-14 02:37:17 -0500

    Added toggleswitch to JS

---
+++ script.js
@@ -0,0 +1,12 @@
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const project1info=  document,getElementById("project1info");
+const project2info=  document,getElementById("project2info");
+
+function toggle(){
+    if (projectinfo.styleSheets.display === "none"){
+        projectinfo.style.display ="block";
+    }else{
+        projectinfor.style.display = "none";
+    }
+}
